e3db4680c7959f6c3f536da9554ada623ec6ff4b
Michael Woodcock <woodcock.michael.a@dol.gov>
2017-03-21 11:22:55 -0400

    Pulling in video thumbnails and titles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
+const API_KEY = 'AIzaSyBcixNoI1IyQkauhS6AbN7R2FsachvhcZA';
 
 //Create a new Component
-const App = () => {
-  return <div>Hi!</div>;
+class App extends Component  {
+  constructor (props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'Surfboards'}, (videos) => {
+      this.setState({ videos })
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 //Take this compentent's generated HTML and put it on the page
